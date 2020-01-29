2097c9949c6c2994fbadf337406e550061fb3fca
khalid241 <33062817+khalid241@users.noreply.github.com>
2020-01-29 17:03:12 +0000

    firstpush

---
+++ Assets/Scripts/Mole.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Mole : MonoBehaviour
+{
+    public GameObject onHit;
+
+    private Score scorescript;
+    
+
+    void Awake()
+    {
+        scorescript = GetComponent<Score>();
+    }
+
+    void OnMouseDown()
+    {
+
+        scorescript.score++;
+        
+        // Load On-Hit effect
+        GameObject g = (GameObject)Instantiate(onHit, transform.position, Quaternion.identity);
+   
+        // Destroy it after a while
+        Destroy(g, 0.2f);
+    }
+    
+}
---
+++ Assets/Scripts/Score.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+ 
+public class Score : MonoBehaviour {
+ 
+    public GameObject onHit;
+    
+    public Text scoreText;
+    public int score;
+
+    
+    // Use this for initialization
+    void Start () {
+   
+    }
+   
+    // Update is called once per frame
+    void Update () {
+ 
+        scoreText.text = "Score: " + score;
+ 
+        if (Input.GetMouseButtonDown(0))
+        {
+            //score++;
+        }
+        
+    }
+}
